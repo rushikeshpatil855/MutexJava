283ccbe34e2f1c96ee8685307728561c2ee59bda
rushikeshpatil855 <30349901+rushikeshpatil855@users.noreply.github.com>
2018-01-22 19:16:44 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -9,22 +9,22 @@ int roll_no[] = new int[num];
 String name[] = new String[num];
 double marks[] = new double[num];
 double percentage[] = new double[num];
-for(i=0;i<=num-1;i++)
+for(i=0;i<num;i++)
 {
-System.out.println("Enter "+i+" Student data");
-System.out.println("Enter Roll no. of "+i+" Student : ");
+System.out.println("Enter "+(i+1)+" Student data");
+System.out.println("Enter Roll no. of "+(i+1)+" Student : ");
 roll_no[i]=sc.nextInt();
-System.out.println("Enter Name of "+i+" Student : ");
+System.out.println("Enter Name of "+(i+1)+" Student : ");
 name[i]=sc.next();
-System.out.println("Enter Marks of "+i+" Student : ");
+System.out.println("Enter Marks of "+(i+1)+" Student : ");
 marks[i]=sc.nextDouble();
-System.out.println("Enter Percentage of :"+i+" Student :");
+System.out.println("Enter Percentage of :"+(i+1)+" Student :");
 percentage[i]=sc.nextDouble();
 }
-for(i=0;i<=num-1;i++)
+for(i=0;i<num;i++)
 {
 System.out.println("Student "+i+" Details ");
-System.out.println("Rollno : "+roll_no[i]+" Name : "+name[i]+" Marks : "+marks[i]+ "Percentage : "+percentage[i]);
+System.out.println("Rollno : "+roll_no[i]+" Name : "+name[i]+" Marks : "+marks[i]+ "Percentage : "+percentage[i]+"%");
 }
 }
 }
